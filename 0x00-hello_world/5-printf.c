a783c6b0f2e200e3f0840f18f832f97cdbc4ecdc
wailabduz <me@example.com>
2023-09-07 18:39:29 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash
- * main - A program that prints a string using printf.
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Always 0.
+ *
+ */
+int	main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
