0fcfacd0c07deb6837bf5b223576692c23d7c8f1
wailabduz <waeelabdu2001@icloud.com>
2023-09-13 17:54:48 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,14 @@
-#include "main.h"
-/**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+#include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	write(STDOUT_FILENO, "_putchar\n", 9);
+	return (0);
 }
