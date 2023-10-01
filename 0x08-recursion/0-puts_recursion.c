fa9521dcaa4ff22e065567362ea1962fcfccc564
wailabduz <waeelabdu2001@icloud.com>
2023-10-01 07:01:13 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-void printStringWithNewLine(char *str) {
-    printf("%s\n", str);
+void _puts_recursion(char *s) {
+    if (*s == '\0') { // Base case: end of string
+        putchar('\n'); // Print a newline character
+        return;
+    }
+    
+    putchar(*s); // Print the current character
+    _puts_recursion(s + 1); // Recursive call with the next character
 }
 
 int main() {
-    char myString[] = "Hello, World!";
-    printStringWithNewLine(myString);
+    char myString[] = "Hello, Recursion!";
+    _puts_recursion(myString);
     return 0;
 }
 
